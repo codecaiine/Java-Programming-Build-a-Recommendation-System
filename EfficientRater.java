e716f0b938382d28a666bfe49335e583efd09fec
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-17 23:25:42 +0000

    Remove unnecessary comments

--- EfficientRater.java
+++ EfficientRater.java
@@ -4,10 +4,8 @@ import java.util.HashMap;
 
 public class EfficientRater implements Rater {
     private String myID;
-    /* Change the ArrayList of type Rating private variable to a HashMap<String,Rating>.
-       The key in the HashMap is a movie ID, and
-        its value is a rating associated with this movie.*/
-    //private HashMap<String, ArrayList<Rating>> myRatings;//why not this one?
+   
+    
     private HashMap<String, Rating> myRatings;
     
     public EfficientRater(String id) {
@@ -16,7 +14,7 @@ public class EfficientRater implements Rater {
     }
     
     public void addRating(String item, double rating) {
-        myRatings.put(item, new Rating(item, rating));//item is string id?
+        myRatings.put(item, new Rating(item, rating));
     }
     
     public boolean hasRating(String item) {
@@ -28,11 +26,7 @@ public class EfficientRater implements Rater {
     }
     
     public double getRating(String item) {
-        //        for (int k = 0; k < myRatings.size(); k++) {
-        //            if (myRatings.get(k).getItem().equals(item)) {
-        //                return myRatings.get(k).getValue();
-        //            }
-        //        }
+     
         if (myRatings.containsKey(item)) {
             return myRatings.get(item).getValue();
         }
@@ -45,10 +39,6 @@ public class EfficientRater implements Rater {
     
     public ArrayList<String> getItemsRated() {
         ArrayList<String> list = new ArrayList<>(myRatings.keySet());
-        //        for (int k = 0; k < myRatings.size(); k++) {
-        //            list.add(myRatings.get(k).getItem());
-        //        }
-        //
         return list;//arrayList of item;
     }
 }

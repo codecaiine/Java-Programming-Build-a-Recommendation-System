543db5cba397288c26a1f45c4a9f7192de0b943d
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-17 23:21:07 +0000

    Update Directors filter

--- DirectorsFilter.java
+++ DirectorsFilter.java
@@ -1,1 +1,22 @@
 
+public class DirectorsFilter implements Filter {
+    private String directors;
+    
+    public DirectorsFilter(String directors) {
+        this.directors = directors;
+    }
+    
+    @Override
+    public boolean satisfies(String id) {
+        String[] dir = directors.split(",");
+        //boolean exist = false;
+        for (String i : dir) {
+            //System.out.println(id + " : " + MovieDatabase.getDirector(id) + " : " + MovieDatabase.getTitle(id));
+            if (MovieDatabase.getDirector(id).contains(i)) {
+                return true;
+                //break;
+            }
+        }
+        return false;
+    }
+}

ead4a7cbac2db477183dd06dbfb949471f877e0a
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-22 23:52:04 +0000

    Remove uunecessary comments Rating.java

--- Rating.java
+++ Rating.java
@@ -3,21 +3,18 @@ public class Rating implements Comparable<Rating> {
     private double value;
     
     public Rating(String anItem, double aValue) {
-        item = anItem;//IMDB id
+        item = anItem;
         value = aValue;
     }
     
-    // Returns item being rated
     public String getItem() {
         return item;
     }
     
-    // Returns the value of this rating (as a number so it can be used in calculations)
     public double getValue() {
         return value;
     }
     
-    // Returns a string of all the rating information
     public String toString() {
         return "[" + getItem() + ", " + getValue() + "]";
     }

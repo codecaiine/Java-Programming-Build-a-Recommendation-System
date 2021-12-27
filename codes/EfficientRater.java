34075c174069568fccbc0703b482a84df15da322
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-27 21:55:16 +0000

    Update EfficientRater file

---
+++ codes/EfficientRater.java
@@ -0,0 +1,52 @@
+
+/**
+ * Write a description of EfficientRater here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+public class EfficientRater implements Rater{
+    private String myID;
+    private HashMap<String, Rating> myRatings;
+
+    public EfficientRater(String id) {
+        myID = id;
+        myRatings = new HashMap<String, Rating>();
+    }
+
+    public void addRating(String item, double rating) {
+        myRatings.put(item, new Rating(item, rating));
+    }
+
+    public boolean hasRating(String item) {
+        return myRatings.containsKey(item);
+    }
+
+    public String getID() {
+        return myID;
+    }
+
+    public double getRating(String item) {
+        if (myRatings.containsKey(item)) {
+            return myRatings.get(item).getValue();
+        } else {
+            return -1;
+        }
+    }
+
+    public int numRatings() {
+        return myRatings.size();
+    }
+
+    public ArrayList<String> getItemsRated() {
+        ArrayList<String> list = new ArrayList<String>();
+        for(String movieID:myRatings.keySet()){
+            list.add(movieID);
+        }
+        
+        return list;
+    }
+}

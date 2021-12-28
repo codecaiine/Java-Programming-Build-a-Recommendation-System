26018baa9b47d00925e897806b9b79115b62ee0f
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-28 17:40:05 +0000

    Remove unnecessary comments GenreFilter.java

--- codes/GenreFilter.java
+++ codes/GenreFilter.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of GenreFilter here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class GenreFilter implements Filter{
     private String genre;
     public GenreFilter(String withGenre){

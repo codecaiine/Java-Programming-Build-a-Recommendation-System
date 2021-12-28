9757a733e2856df0d7601554549a272211c14458
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-28 18:03:12 +0000

    Update YearAfterFilter.java

--- codes/YearAfterFilter.java
+++ codes/YearAfterFilter.java
@@ -1,3 +1,13 @@
-public class YearAfterFilter {
-    
+public class YearAfterFilter implements Filter {
+	private int myYear;
+	
+	public YearAfterFilter(int year) {
+		myYear = year;
+	}
+	
+	@Override
+	public boolean satisfies(String id) {
+		return MovieDatabase.getYear(id) >= myYear;
+	}
+
 }

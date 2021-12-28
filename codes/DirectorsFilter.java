a657acf654683068131295c6eef67acc6c7f0f13
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-28 17:43:15 +0000

    Remove unnecessary comments DirectorsFilter.java

--- codes/DirectorsFilter.java
+++ codes/DirectorsFilter.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of DirectorsFilter here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class DirectorsFilter implements Filter{
     private String[] dirList;
     

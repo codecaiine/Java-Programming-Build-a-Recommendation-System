7500b905f17e0d7e29e0365d4aff07a03056e00e
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-28 18:07:26 +0000

    Remove unnecessary Rating.java

--- codes/Rating.java
+++ codes/Rating.java
@@ -7,17 +7,14 @@ public class Rating implements Comparable<Rating> {
         value = aValue;
     }
 
-    // Returns item being rated
     public String getItem () {
         return item;
     }
 
-    // Returns the value of this rating (as a number so it can be used in calculations)
     public double getValue () {
         return value;
     }
 
-    // Returns a string of all the rating information
     public String toString () {
         return "[" + getItem() + ", " + getValue() + "]";
     }

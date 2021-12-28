a0a85390a9619b4c904e52e00b39d03ca57f884f
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-28 17:35:01 +0000

    Remove unnecessary comments MovieRunnerAverage.java

--- codes/MovieRunnerAverage.java
+++ codes/MovieRunnerAverage.java
@@ -1,11 +1,3 @@
-
-/**
- * Write a description of MovieRunnerAverage here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-
 import java.util.*;
 
 public class MovieRunnerAverage {

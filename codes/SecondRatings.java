0d66e11de4a855e7e6edfcbbef801272155fafa2
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-28 18:04:45 +0000

    Remove comments SecondRatings.java

--- codes/SecondRatings.java
+++ codes/SecondRatings.java
@@ -1,11 +1,3 @@
-
-/**
- * Write a description of SecondRatings here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-
 import edu.duke.*;
 import java.util.*;
 import org.apache.commons.csv.*;

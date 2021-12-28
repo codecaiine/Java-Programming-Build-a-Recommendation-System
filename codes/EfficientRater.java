f95fd6f149a0cb9b6b88bc8da018d683c288799b
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-28 17:42:28 +0000

    Remove unnecessary comments EfficientRater.java

--- codes/EfficientRater.java
+++ codes/EfficientRater.java
@@ -1,11 +1,3 @@
-
-/**
- * Write a description of EfficientRater here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-
 import java.util.*;
 
 public class EfficientRater implements Rater{

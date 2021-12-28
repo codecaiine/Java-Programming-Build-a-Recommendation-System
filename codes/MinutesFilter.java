3b8f3b179c27ad2d5217c8deb7f5ee3a762b201f
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-28 17:38:59 +0000

    Remove unnecessary comments MinutesFilter.java

--- codes/MinutesFilter.java
+++ codes/MinutesFilter.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of MInutesFilter here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MinutesFilter implements Filter{
     private int min;
     private int max;
